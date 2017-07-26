2242735c3c954b9e5fa44c2181e718399d272821
lbellows <lbellows@gmail.com>
2017-07-25 21:20:59 -0400

    initial

---
+++ README.md
@@ -0,0 +1,30 @@
+#CloudFlare DDNS example
+This will check your external ip every X minutes and update your account.  You can use the info.js to get your account information to use the config example below after filling in your account section.  You can also add multiple updates in the interval by duplicating the line below and updating your config
+```
+cfDNS.UpdateDomainDNS(...)
+```
+
+##Config outline.
+You can get the keys from your cloudflare account or using the script.
+{
+	"account":{
+		"email": "",
+		"key": ""
+	},
+	"records":[
+		{
+			"type": "", (A)
+			"key": "", (subdomain)
+			"domainKey": "",
+			"recordKey": ""
+		},
+		{ ... }
+	],
+	"minuteInterval": 60
+}
+
+##Running the program
+After updating the config:
+```
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,41 @@
+var con = require('manakin').local;
+var cf = require('./CloudFlareDDNS');
+var os = require('os');
+var config = require('./config.json');
+
+con.warn.bright = true; // use bright yellow for `console.warn`
+con.error.bright = true; // use bright red for `console.error`
+con.success.bright = true; // use bright green for `console.success`
+con.info.bright = true; // use bright cyan for `console.info`
+con.log.bright = true; // use bright cyan for `console.info`
+
+con.warn('Starting...');
+var cfDNS = new cf(config.account.email, config.account.key);
+var mins = config.minuteInterval;
+var counter = 1;
+
+cf.PrintProgress(`[Minute(s) until next update] 0\\${mins}`);
+
+setInterval(() => {
+	cf.GetExternalIp().then(ip => {
+		con.log();
+		Promise.all(
+			config.records.map(record=> {
+				return cfDNS.UpdateDomainDNS(record.type, record.key, ip, record.domainKey, record.recordKey)
+					.then(res => {
+						con.log(`[Updated ${res.data.result.name} -> ${res.data.result.content}] ${new Date().toLocaleString()}`);
+					})
+			})
+		).then(() => {
+			cf.PrintProgress(`[Minute(s) until next update] 0\\${mins}`);
+		})
+	})
+}, mins * 60000);
+
+setInterval(() => {
+	cf.PrintProgress(`[Minute(s) until next update] ${counter}\\${mins}`);
+	if(counter >= mins)
+		counter = 1;
+	else
+		counter += 1;
+}, 60000);
